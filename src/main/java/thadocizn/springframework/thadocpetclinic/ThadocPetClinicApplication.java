ba32bc25c07b5ad48d9e0221ffb9d3ad718f4849
ThaDocIzN <rodan72@msn.com>
2019-06-23 12:53:43 -0500

    initial commit

---
+++ src/main/java/thadocizn/springframework/thadocpetclinic/ThadocPetClinicApplication.java
@@ -0,0 +1,13 @@
+package thadocizn.springframework.thadocpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThadocPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThadocPetClinicApplication.class, args);
+    }
+
+}
